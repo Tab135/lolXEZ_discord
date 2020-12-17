6bbe54b71790a9a8bfda2be0670d9534e7e20ecb
Tab135 <70458035+Tab135@users.noreply.github.com>
2020-12-17 23:07:41 +0700

    Update homie.py

--- Cogs/homie.py
+++ Cogs/homie.py
@@ -39,6 +39,29 @@ class homie(commands.Cog):
         await ctx.message.delete()
         await ctx.send(f"Hmmm someone accused {user} of {something} {smt2} {smt3} {smt4} {smt5} {smt6} ")
 
+  #######################################################################################################################################################################################################################################################################
+    @commands.command()
+    async def tặng(self, ctx, member, gift, part2: commands.clean_content = None,part3: commands.clean_content = None ):
+     gift2 = f"**{part2}** " if part2 else ""
+     gift3 = f"**{part3}** " if part3 else ""
+     await ctx.send(f"**{ctx.author.name}** đã tặng **{member}** cái **{gift}** {gift2} {gift3} ")
+
+    @commands.command()
+    async def đâm(self,ctx,user):
+        respone = [f'Chết rồi  **{ctx.author.name}** đã đâm **{user}** ',
+                   f' **{user}** đã lật ngược tình thế lại và đâm chết  **{ctx.author.name}** ',
+                   f'Vãi thật **{ctx.author.name}** tự đâm trúng mình 🤦 ',
+                   f'Vãi **{user}** bị đâm chết rồi ai cứu nó đi ']
+        await ctx.send(random.choice(respone))
+    @commands.command()
+    async def đổ_tội(self,ctx,user,something,something2: commands.clean_content = None,something3: commands.clean_content = None,something4: commands.clean_content = None,something5: commands.clean_content = None,something6: commands.clean_content = None):
+        smt2 = something2 if something2 else ""
+        smt3 = something3 if something3 else ""
+        smt4 = something4 if something4 else ""
+        smt5 = something5 if something5 else ""
+        smt6 = something6 if something6 else ""
+        await ctx.message.delete()
+        await ctx.send(f"Hmmm ai đó đổ tội {user} đã {something} {smt2} {smt3} {smt4} {smt5} {smt6}")
 
 def setup(bot):
     bot.add_cog(homie(bot))
