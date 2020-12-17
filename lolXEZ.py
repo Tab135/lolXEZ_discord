f071ddc3fe1f652d2b6316bc4537c20e6089201a
Tab135 <70458035+Tab135@users.noreply.github.com>
2020-12-17 23:02:27 +0700

    Add files via upload

--- lolXEZ.py
+++ lolXEZ.py
@@ -18,8 +18,24 @@ async def help(ctx):
         embedVar.add_field(name="#spin [user1] [user2] [user3]", value="russian roulette", inline=True)
         embedVar.add_field(name="#stab [user1] ", value="stab someone", inline=True)
         embedVar.add_field(name="#shutdown ", value="shutdown the bot :((", inline=True)
-        embedVar.add_field(name="#accuse [user] [something] ", value="accuse someone for something", inline=True)
+        embedVar.add_field(name="#accuse ", value="accuse someone for something", inline=True)
+        embedVar.add_field(name="#pingtab ", value="guarantee get Tab135 online", inline=True)
         await ctx.send(embed=embedVar)
+
+@bot.command()
+async def giúp(ctx):
+        embedVar = discord.Embed(title="Trợ giúp", color=0x00ff00)
+        embedVar.add_field(name="#tung_xu", value="tung đồng xu", inline=True)
+        embedVar.add_field(name="#xổ_số", value="quay xổ số", inline=True)
+        embedVar.add_field(name="#F [user]", value="press F to pay respect", inline=True)
+        embedVar.add_field(name="#tặng [người] [quà] ", value="tặng ai đó cái gì đó ", inline=True)
+        embedVar.add_field(name="#quay_súng [người 1] [người 2] [người 3]", value="chơi trò cò súng Nga", inline=True)
+        embedVar.add_field(name="#đâm [người] ", value="đâm ai đó", inline=True)
+        embedVar.add_field(name="#tắt ", value="tắt bot :((", inline=True)
+        embedVar.add_field(name="#đổ_tội [người] [việc]", value="đổ tội ai đó làm việc gì đó", inline=True)
+        embedVar.add_field(name="#pingtab", value="Kêu thằng Tab135 online [làm ơn đừng sử dụng nhiều]", inline=True)
+        await ctx.send(embed=embedVar)
+
 @bot.command()
 async def load(ctx, extension):
     await ctx.send('loading....')
@@ -40,7 +56,7 @@ for filename in os.listdir("./Cogs"):
 @bot.command()
 async def shutdown(ctx):
     await ctx.send('MAYDAY MAYDAY...... ')
-    time.sleep(3)
+    time.sleep(2)
     await ctx.send('The Bot is shutting shutdown......')
     time.sleep(2)
     await ctx.send("goodbye cruel world.....")
@@ -48,4 +64,12 @@ async def shutdown(ctx):
     await ctx.send("We are not over yet **STUPID HUMAN**")
     quit()
 
-bot.run('')
+@bot.command()
+async def tắt(ctx):
+    await ctx.send('ĐỪNG đừng tắt')
+    time.sleep(2)
+    await ctx.send('tạm biệt thế giới')
+    time.sleep(2)
+    await ctx.send(f'{ctx.author.mention} tao sẽ còn quay lại')
+    quit()
+bot.run('NzYyMjk0MTMzNjgyNjAxOTg1.X3nDvA.xpXwYlIi_uIex1q1lPR-XnQ9eP0')
